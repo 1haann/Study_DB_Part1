ffb4f2e45a0dfc03c8ac53be2e2190af07830c66
Han <inte098@naver.com>
2024-11-11 03:53:48 +0900

    섹션2 - JDBC 개발(수정,삭제)

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,9 +6,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -18,7 +18,7 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV5", 10000);
+        Member member = new Member("memberV8", 10000);
         repository.save(member);
 
         //findById
@@ -28,5 +28,15 @@ class MemberRepositoryV0Test {
         // NOTE findById 메소드에서 새로운 인스턴스를 생성하여 반환하기에
         //  findMember와 member는 다른 인스턴스이다. 그럼에도 true가 나오는 이유는
         //  @Data 어노테이션이 EqualsAndHashCode를 자동으로 만들어주기 때문이다.
+
+        //update: money: 10000 -> 2000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }

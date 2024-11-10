0f9432ebb1dfccb33d23bceaac8e24d33034f645
Han <inte098@naver.com>
2024-11-11 01:26:56 +0900

    섹션2 - JDBC 개발(조회)

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,19 +1,32 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        //save
+        Member member = new Member("memberV5", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+        // NOTE findById 메소드에서 새로운 인스턴스를 생성하여 반환하기에
+        //  findMember와 member는 다른 인스턴스이다. 그럼에도 true가 나오는 이유는
+        //  @Data 어노테이션이 EqualsAndHashCode를 자동으로 만들어주기 때문이다.
     }
 }
